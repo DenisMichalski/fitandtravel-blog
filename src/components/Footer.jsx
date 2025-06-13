f67403c7c85695d6c7a770340e75b18da3cc473c
Denis11m <denissmi87@gmail.com>
2025-06-13 02:58:48 +0200

    neue Datei Fotter.jsx für impressum und Datenschutz

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <footer className='bg-slate-900 text-white text-center py-8 mt-16'>
+      <div className='flex flex-col md:flex-row justify-center items-center gap-6'>
+        <span>&copy; {newDate().getFullYear()} Fit &amp; Travel Blog</span>
+        <Link
+          to='/blog/impressum'
+          className='hover:undeline hover:text-blue-300 transition'
+        >
+          Impressum
+        </Link>
+        <Link
+          to='/blog/datenschutz'
+          className='hover:underline hover:text-blue-300 transition'
+        >
+          Datenschutz
+        </Link>
+      </div>
+      <div className='mt-4 text-sm text-gray-400'>
+        Powered by React &amp; Tailwind CSS
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
