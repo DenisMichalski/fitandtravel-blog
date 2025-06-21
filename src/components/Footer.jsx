e43d0a9b0267cae0e445a2af213a969e83e341c8
Denis11m <denissmi87@gmail.com>
2025-06-21 03:11:06 +0200

    Meinen Namen in den Footer hinzugefügt

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className='bg-slate-900 text-white text-center py-8 mt-16 shadow-lg'>
       <div className='flex flex-col md:flex-row justify-center items-center gap-6'>
-        <span>&copy; {new Date().getFullYear()} Fit &amp; Travel Blog</span>
+        <span>&copy; {new Date().getFullYear()} Fit &amp; Travel Blog · Ein Projekt von Denis Michalski</span>
         <Link
           to='/impressum'
           className='hover:underline hover:text-blue-300 transition'
@@ -17,14 +17,14 @@ function Footer() {
         >
           Datenschutz
         </Link>
-      </div>
-      <Link
-        to='/kontakt'
-        className='hover:underline hover:text-blue-300 transition'
-      >
-        Kontakt
-      </Link>
 
+        <Link
+          to='/kontakt'
+          className='hover:underline hover:text-blue-300 transition'
+        >
+          Kontakt
+        </Link>
+      </div>
       <Link>
         <div className='mt-4 text-sm text-gray-400'>
           Powered by React &amp; Tailwind CSS
