97112251af66087ed8a0920a9117da0c182ca15e
Denis11m <denissmi87@gmail.com>
2025-06-21 03:48:43 +0200

    Footer verbessert

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,20 @@ function Footer() {
   return (
     <footer className='bg-slate-900 text-white text-center py-8 mt-16 shadow-lg'>
       <div className='flex flex-col md:flex-row justify-center items-center gap-6'>
-        <span>&copy; {new Date().getFullYear()} Fit &amp; Travel Blog · Ein Projekt von Denis Michalski</span>
-        
+        <div>
+          © 2025 Fit & Travel Blog &nbsp;|&nbsp;
+          by <span className="font-bold text-pink-500">Denis Michalski</span>
+          &nbsp;•&nbsp;
+          <a
+            href="https://www.instagram.com/fitandtravelblog/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline hover:text-yellow-400"
+          >
+            Instagram
+          </a>
+        </div>
+
         <Link
           to='/impressum'
           className='hover:underline hover:text-blue-300 transition'
