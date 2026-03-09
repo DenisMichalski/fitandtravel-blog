b9d73ebdc2d68c2d2fc3876ae60507e109e4f49f
Denis11m <denissmi87@gmail.com>
2026-03-09 13:38:21 +0100

    Entfernen von toten Code und kleineren UI-Fehlern.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className='bg-slate-900 text-white text-center py-8 mt-16 shadow-lg'>
-      <div className='flex flex-col md:flex-row justify-center items-center gap-6'>
+    <footer className="bg-slate-900 text-white text-center py-8 mt-16 shadow-lg">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-6">
         <div>
-          © 2025 Fit & Travel Blog &nbsp;|&nbsp;
-          by <span className="font-bold text-pink-500">Denis Michalski</span>
+          © 2025 Fit & Travel Blog &nbsp;|&nbsp; by{" "}
+          <span className="font-bold text-pink-500">Denis Michalski</span>
           &nbsp;•&nbsp;
           <a
             href="https://www.instagram.com/fitandtravelblog/"
@@ -19,30 +19,28 @@ function Footer() {
         </div>
 
         <Link
-          to='/impressum'
-          className='hover:underline hover:text-blue-300 transition'
+          to="/impressum"
+          className="hover:underline hover:text-blue-300 transition"
         >
           Impressum
         </Link>
         <Link
-          to='/datenschutz'
-          className='hover:underline hover:text-blue-300 transition'
+          to="/datenschutz"
+          className="hover:underline hover:text-blue-300 transition"
         >
           Datenschutz
         </Link>
 
         <Link
-          to='/kontakt'
-          className='hover:underline hover:text-blue-300 transition'
+          to="/kontakt"
+          className="hover:underline hover:text-blue-300 transition"
         >
           Kontakt
         </Link>
       </div>
-      <Link>
-        <div className='mt-4 text-sm text-gray-400'>
+        <div className="mt-4 text-sm text-gray-400">
           Powered by React &amp; Tailwind CSS
         </div>
-      </Link>
     </footer>
   );
 }
